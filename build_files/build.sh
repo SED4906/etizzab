5397d6f9cb1f8ac9753b2e5b8800f8f243af81df
SED4906 <sed4906birdie@gmail.com>
2026-02-02 05:15:10 -0800

    nuclear fusion

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,10 +5,10 @@ set -ouex pipefail
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
+# RPMfusion repos are available by default in ublue main images (I'm afraid not)
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
+dnf5 install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 # this installs a package from fedora repos
 dnf5 install -y tmux kvantum mpd
 
